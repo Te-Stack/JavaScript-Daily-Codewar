3a5feb50aeb568b2b1d567517292bb8e63a51744
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-02-19 09:44:44 +0100

    Data Structure and Algorithm

--- Algorithm/A.js
+++ Algorithm/A.js
@@ -8,4 +8,27 @@ const arrAsc = (numArray)=>{
     })
 }
 
-console.log(arrAsc([2,3,4,7,2,4]))
+// console.log(arrAsc([2,3,4,7,2,4]))
+
+// Function to check if the square of elements is in another array
+const arrSquare = (arr1, arr2)=>{
+    // First Check if both arrays are of the same length
+    if(arr1.length !== arr2.length){
+        return false
+    }
+
+    const compArr1 = arrAsc(arr1)
+    const compArr2 = arrAsc(arr2)
+
+    // Check if element is second array is equal to the square of corresponding element in first array 
+    for(let i = 0; i<compArr1.length; i++){
+        if(compArr2[i] !== Math.pow(compArr1[i], 2)){
+            return false
+        }
+    }
+    return true
+
+}
+
+console.log(arrSquare([1,2,3],[4,1,9]))
+console.log(arrSquare([1,2,4],[4,1,16]))
