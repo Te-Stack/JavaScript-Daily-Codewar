014c58eb7b7093f217be6d8b8ee5cac987ac7f5b
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-02-19 09:20:44 +0100

    Data Structure and Algorithm

--- Algorithm/A.js
+++ Algorithm/A.js
@@ -1,2 +1,11 @@
-// Write a Function callede same,which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must  be same.
+// Write a Function called same,which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must  be same.
 
+
+// Function to sort array of integers in ascending order
+const arrAsc = (numArray)=>{
+    return numArray.sort((a,b)=>{
+        return a -b
+    })
+}
+
+console.log(arrAsc([2,3,4,7,2,4]))
